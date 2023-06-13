826708d79130c5f9730424897d65dbbcc3f967f7
Elvis <eleram@protonmail.com>
2023-06-13 01:02:41 -0300

    Controller de criação de academia

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import { ZodError } from 'zod'
 import fastifyJwt from '@fastify/jwt'
 
 import { env } from '@/env'
-import { appRoutes } from '@/http/routes'
+
+import { usersRoutes } from '@/http/controllers/users/routes'
+import { gymsRoutes } from '@/http/controllers/gyms/routes'
 
 export const app = fastify()
 
@@ -11,9 +13,10 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
 
-app.setErrorHandler((error, _request, reply) => {
+app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
